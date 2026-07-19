5c19f2b01063a7e84995a9e8f78a18da15dec0a1
Eze Larry Ifeanyi <ezelarry379@gmail.com>
2026-07-19 01:28:00 +0100

    working on notification,order and payment servers

--- OderService/src/main/java/com/ifeanyi/OderService/Repository/OrderRepository.java
+++ OderService/src/main/java/com/ifeanyi/OderService/Repository/OrderRepository.java
@@ -1,6 +1,7 @@
 package com.ifeanyi.OderService.Repository;
 
 import com.ifeanyi.OderService.entity.Order;
+import com.ifeanyi.OderService.entity.OrderStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -11,4 +12,5 @@ public interface OrderRepository extends MongoRepository<Order, String> {
     Page<Order> findAllByUserIdOrProductId(String userId, String productId, Pageable pageable);
 
     Page<Order> findAllByUserId(String userId, Pageable pageable);
+    Page<Order> findAllByUserIdAndStatus(String userId, OrderStatus status, Pageable pageable);
 }
