484cbe90eae170441dbff041ec524ee01bb8cffc
Eze Larry Ifeanyi <ezelarry379@gmail.com>
2026-08-23 01:22:18 +0100

    added reedme file

--- Gateway/src/main/java/com/ifeanyi/Gateway/config/GatewayRoutingConfig.java
+++ Gateway/src/main/java/com/ifeanyi/Gateway/config/GatewayRoutingConfig.java
@@ -1,10 +1,15 @@
 package com.ifeanyi.Gateway.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GatewayRoutingConfig {
 
-    RouteLoc
+    @Bean
+  public   RouteLocator
 
 }
